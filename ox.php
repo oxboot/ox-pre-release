17ae8e66ed7ba2e42cae79b01089709367af8484
Zorca <vs@zorca.org>
2017-09-09 02:48:49 +0300

    Version constant added

--- ox.php
+++ ox.php
@@ -1,10 +1,12 @@
 #!/usr/bin/env php
 <?php
-define('OX_DIR', __DIR__);
+define('DS', '/');
+define('OX_ROOT', str_replace(DIRECTORY_SEPARATOR, DS, __DIR__ . DS));
+define('OX_VERSION', '0.0.0');
 
-require OX_DIR . '/vendor/autoload.php';
+require OX_ROOT . 'vendor/autoload.php';
 
-$application = new \Symfony\Component\Console\Application();
+$application = new \Symfony\Component\Console\Application('ox ' . OX_VERSION);
 
 $application->add(new Ox\Command\SiteCreateCommand());
 $application->add(new Ox\Command\SiteDeleteCommand());
