f0a88a246077a54bd0dca71b21ae61c413416b2f
Zorca <vs@zorca.org>
2017-09-09 01:03:25 +0300

    Options added to SiteCreateCommand

--- src/Command/SiteCreateCommand.php
+++ src/Command/SiteCreateCommand.php
@@ -2,6 +2,12 @@
 namespace Ox\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
 
 class SiteCreateCommand extends Command
 {
@@ -11,6 +17,17 @@ class SiteCreateCommand extends Command
             ->setName('site:create')
             ->setDescription('Create a new site')
             ->setHelp('This command allows you to create a new site')
+            ->addArgument('site_name', InputArgument::REQUIRED, 'Name of the site')
+            ->addOption('php', null, InputOption::VALUE_NONE, 'PHP support')
+            ->addOption('mysql', null, InputOption::VALUE_NONE, 'MySQL support')
         ;
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $site_name = $input->getArgument('site_name');
+        $php_support = $input->getOption('php');
+        $mysql_support = $input->getOption('mysql');
     }
 }
