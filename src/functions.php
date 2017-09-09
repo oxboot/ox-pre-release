7bb04efa4910e2b27c9a8590a85e176ae34e88ab
Zorca <vs@zorca.org>
2017-09-09 20:27:16 +0300

    Echo & Exec functions added

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,34 @@
 <?php
+
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+function ox_echo($message, $color = 'white')
+{
+    $output = new ConsoleOutput();
+    $output->writeln('<fg=' . $color .'>' . $message . '</>');
+}
+
+function ox_echo_success($message)
+{
+    ox_echo($message, 'green');
+}
+
+function ox_echo_error($message)
+{
+    ox_echo($message, 'red');
+}
+
+function ox_exec($command)
+{
+    $process = new Process($command);
+    try {
+        $process->mustRun();
+        ox_echo($process->getOutput());
+        return true;
+    } catch (ProcessFailedException $e) {
+        ox_echo_error($e->getMessage());
+        return false;
+    }
+}
